da878ce0b60b7605a464ee0ee852c636413a98f7
Dragonfly <dragonfly@MacBook-Pro-2.local>
2025-08-14 10:31:10 +0700

    edit all file main and add check empty domain

--- main.py
+++ main.py
@@ -20,14 +20,6 @@ def detect_file_type(file_path):
     else:
         return 'unknown'
     
-def check_ip_group(ip):
-    reader = csv.reader(open('group.csv', newline='', encoding='utf-8'))
-    for row in reader:
-        prefix = row[0]
-        group = row[1]
-        if ip.startswith(prefix):
-            return f',{group}'
-    return ',Unknown'
         
 def check_ip_group(ip):
     reader = csv.reader(open('group.csv', newline='', encoding='utf-8'))
@@ -87,6 +79,7 @@ def valid_domain(domain):
         return False
     return True
 
+
 def write_output_to_file(output_string, output_path):
     
     file_exists = os.path.exists(output_path)
@@ -101,6 +94,21 @@ def write_output_to_file(output_string, output_path):
             row = [col.strip() for col in line.split(',')]
             writer.writerow(row)
             
+def check_empty_domain_to_ip(domain):
+    if not valid_domain(domain):
+        return "continue"
+    try:
+        if pd.notna(domain) and domain != "":
+            result = subprocess.run(['ping', '-c', '1', domain], capture_output=True, text=True, timeout=2)
+            if(result.returncode == 0):
+                match = re.search(r'\(([\d\.]+)\)', result.stdout)
+                if match:
+                    ip = match.group(1)
+                    return ip
+        return "CantPing"
+    except Exception as e:
+        return ""
+    
 
 def scan_ip(reader):
     global output_string, output_failed
@@ -109,46 +117,35 @@ def scan_ip(reader):
         domain2 = row[18]
         domain3 = row[19]
         domain4 = row[20]
-        print("Checking valid domain....")
+        # print("Checking valid domain....")
         if not valid_domain(domain1) and not valid_domain(domain2) and not valid_domain(domain3) and not valid_domain(domain4):
+            print("not ok")
             continue
-            print("Checked valid domain")
-        try:
-            result = subprocess.run(['ping', '-c', '1', domain1], capture_output=True, text=True, timeout=2)
-            time.sleep(time_delay)
-            result2 = subprocess.run(['ping', '-c', '1', domain2], capture_output=True, text=True, timeout=2)
-            time.sleep(time_delay)
-            result3 = subprocess.run(['ping', '-c', '1', domain3], capture_output=True, text=True, timeout=2)
-            time.sleep(time_delay)
-            result4 = subprocess.run(['ping', '-c', '1', domain4], capture_output=True, text=True, timeout=2)
-            if result.returncode == 0 and result2.returncode == 0 and result3.returncode == 0 and result4.returncode == 0:
-                match = re.search(r'\(([\d\.]+)\)', result.stdout)
-                match2 = re.search(r'\(([\d\.]+)\)', result2.stdout)
-                match3 = re.search(r'\(([\d\.]+)\)', result3.stdout)
-                match4 = re.search(r'\(([\d\.]+)\)', result4.stdout)
-                if match and match2 and match3 and match4:
-                    ip = match.group(1)
-                    ip2 = match2.group(1)
-                    ip3 = match3.group(1)
-                    ip4 = match4.group(1)
-                    group_domain = f"{check_ip_group_v2(ip)},{check_ip_group_v2(ip2)},{check_ip_group_v2(ip3)},{check_ip_group_v2(ip4)}"
-                    if compare_ip_groups(check_ip_group_v2(ip), check_ip_group_v2(ip2), check_ip_group_v2(ip3), check_ip_group_v2(ip4)):
-                        group_domain += ',NonCross'
-                    else:
-                        group_domain += ',Cross'
-
-                else:
-                    print(f"{domain1} -> No IP found")
-            else:
-                output_failed += f"{domain1},NotExist\n"
-            # print(group_domain)
-            write_output_to_file(group_domain, 'output.csv')        
-        except Exception as e:
-            print(f"{domain1} -> Error: {e}")
+        # print(f"{check_empty_domain_to_ip(domain1)},{check_empty_domain_to_ip(domain2)},{check_empty_domain_to_ip(domain3)},{check_empty_domain_to_ip(domain4)}")
+        ip1 = check_empty_domain_to_ip(domain1)
+        ip2 = check_empty_domain_to_ip(domain2)
+        ip3 = check_empty_domain_to_ip(domain3)
+        ip4 = check_empty_domain_to_ip(domain4)
+        #    group_ip = f"{check_empty_domain_to_ip(domain1)},{check_empty_domain_to_ip(domain2)},{check_empty_domain_to_ip(domain3)},{check_empty_domain_to_ip(domain4)}"
+        if(check_ip_group_v2(ip1) == check_ip_group_v2(ip2) and check_ip_group_v2(ip1) == check_ip_group_v2(ip3) and check_ip_group_v2(ip1) == check_ip_group_v2(ip4)):
+            group_ip =  f"{check_ip_group_v2(ip1)},{check_ip_group_v2(ip2)},{check_ip_group_v2(ip3)},{check_ip_group_v2(ip4)},NonCross"
+            # print(group_ip)
+        else:
+            group_ip = f"{check_ip_group_v2(ip1)},{check_ip_group_v2(ip2)},{check_ip_group_v2(ip3)},{check_ip_group_v2(ip4)},Cross"
+            # print(group_ip)
+        output_string += group_ip + '\n'
 
 
+
+
+
+def export_to_xlsx(input_path, output_path):
+    df = pd.read_csv(input_path)
+    df.to_excel(output_path, index=False, engine='openpyxl')
+    print(f"Exported to {output_path}")
 
 # main
+option_export_file_ip = input("Do you want to export ip file? (y/n): ")[0]   
 
 if detect_file_type(file_path) == 'xlsx':
     df = pd.read_excel(file_path)
@@ -158,8 +155,16 @@ elif detect_file_type(file_path) == 'csv':
     with open(file_path, newline='', encoding='utf-8') as csvfile:
         reader_csv = csv.reader(csvfile)
         scan_ip(reader_csv)
+write_output_to_file(output_string, '/Users/dragonfly/Documents/scan_ip/output/output.csv')
 
 
+# export to xlsx
+csv_file = '/Users/dragonfly/Documents/scan_ip/output/output.csv'
+xlsx_file = '/Users/dragonfly/Documents/scan_ip/output/xlsx/output.xlsx'
+
+option_export_file_xlsx = input("Do you want to export output to xlsx file? (y/n): ")[0]
+if option_export_file_xlsx.lower() == 'y':
+    export_to_xlsx(csv_file, xlsx_file)
 
     
 
